a601666eb020e7a4726f0942c3f73a9b7a11de69
mbarrionuevo <matias.barrionuevo@settlenetwork.com>
2022-05-22 18:28:51 -0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import MetaTags from "react-meta-tags";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <MetaTags>
+      <title>Page 1</title>
+      <meta name="description" content="Some description." />
+      <meta property="og:title" content="MyApp" />
+      <meta property="og:image" content={`${window.location.href}assets/logo.jpg`} />
+    </MetaTags>
     <App />
   </React.StrictMode>
-)
+);
